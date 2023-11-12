d37c3799e01b12b64080417f192601ca5ac62379
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-11-12 13:32:22 +0300

    added sass options to pre-import global variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  sassOptions: {
+    additionalData: `@import "src/assets/styles/variables.scss";`,
+  },
 };
 
 module.exports = nextConfig;
