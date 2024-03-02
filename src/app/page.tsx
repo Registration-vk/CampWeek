4df3a1144dc7d6b5d29e553bd459818aaa63b993
Alexey Mitin <adduction@mail.ru>
2024-03-02 13:56:45 +0300

    correct mistake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { useEventsAll } from "@/core/hooks";
 import styles from "./styles.module.scss";
 import { MainBanner } from "@/components/ui/MainBanner/MainBanner";
 import { Title } from "@/components/ui/Title/Title";
-import { SmallCard } from "@/components/ui/SmallCard/SmallCard";
 import { Carousel } from "@/components/ui/Carousel/Carousel";
 
 export default function Home() {
@@ -20,8 +19,8 @@ export default function Home() {
       {isError && <h5>При загрузке данных произошла ошибка</h5>}
       <div className={styles.eventsWrapper}>
         <h2 className={styles.title}>Программа</h2>
-        {/* {events && events.length > 0 && <Carousel events={events} />} */}
-        {events && (
+        {events && events.length > 0 && <Carousel events={events} />}
+        {!events && (
           <div className={styles.noEventWrapper}>
             <h3 className={styles.noEventTitle}>Совсем скоро здесь появятся наши мероприятия</h3>
             <p className={styles.noEventText}>А пока вы можете заявить о своём мероприятии</p>
